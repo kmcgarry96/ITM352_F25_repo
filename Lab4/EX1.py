e4c9e578ae7410e2674879a2a55947fe8dd34605
kmcgarry96 <kmcgarry@hawaii.edu>
2025-09-12 13:03:05 -1000

    Update EX1.py with string manipulation and f-string example

---
+++ Lab4/EX1.py
@@ -0,0 +1,11 @@
+# string manipulation example 
+
+first = input ("Enter your first name: ")
+middle_initial = input ("Enter your middle initial: ")
+last = input ("Enter your last name: ")
+
+full_name = last + " " + first 
+print("Your full name is:", full_name)
+
+print (f"your full name is {last} {first} {middle_initial}")
+
