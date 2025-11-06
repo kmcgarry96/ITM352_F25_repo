cd179e1bc201347930cf1a0b231d6d190cbb1d17
kmcgarry96 <kmcgarry@hawaii.edu>
2025-11-05 21:20:26 -1000

    Add Lab12 web scraping exercises with working Python scripts

---
+++ Lab12/ex1.py
@@ -0,0 +1,20 @@
+# Open a URL from city of chiago data portal and print some content from it.
+import urllib.request
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+url = "https://data.cityofchicago.org/Historic-Preservation/Landmark-Districts/zidz-sdfj/about_data"
+encoding = 'utf-8'
+
+# open the web page
+print(f"Opening {url}...")
+webpage = urllib.request.urlopen(url)
+
+# iterate through each line in the webpage and search for <title>
+for line in webpage:
+    line = line.decode(encoding)
+    if '<label>' in line:
+        print(line)
+
+
